a77b27b19efcb8822fa71d48ff9bd5c23af9f3a1
sasanadya <sasanadyarizki2@gmail.com>
2022-09-18 19:56:30 +0700

    add class loginForm

---
+++ WeatherMelon/WeatherMelon/loginForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class loginForm
+{
+    public string username { get; set; }
+    public string password { get; set; }
+    public string connString;
+
+    public Boolean userCheck(string username, string password)
+    {
+        return true;
+    }
+
+}
