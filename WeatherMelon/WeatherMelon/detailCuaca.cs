5ec446e638a3c275290f604275b211594afb798d
khalidrizki01 <khalidrizki01@mail.ugm.ac.id>
2022-10-25 09:25:13 +0700

    make new solution

--- WeatherMelon/WeatherMelon/detailCuaca.cs
+++ WeatherMelon/WeatherMelon/detailCuaca.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class detailCuaca
+namespace WeatherMelon
 {
-	public detailCuaca()
-	{
-		//Attributes here
+    internal class detailCuaca
+    {
 
-		//getAttributes()
-	}
+    }
 }
--- WeatherMelon/WeatherMelon/mainForm.cs
+++ WeatherMelon/WeatherMelon/mainForm.cs
@@ -1,3 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace WeatherMelon
 {
     public partial class mainForm : Form
@@ -6,7 +16,5 @@ namespace WeatherMelon
         {
             InitializeComponent();
         }
-
-        detailCuaca dtl = new detailCuaca();
     }
 }
