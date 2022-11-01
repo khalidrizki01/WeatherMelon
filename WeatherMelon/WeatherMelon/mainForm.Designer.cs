60aec0a429c1a1d35c65ab84caa9216d1b696cfe
khalidrizki01 <khalidrizki01@mail.ugm.ac.id>
2022-11-01 10:40:18 +0700

    Add days boxes

--- WeatherMelon/WeatherMelon/mainForm.Designer.cs
+++ WeatherMelon/WeatherMelon/mainForm.Designer.cs
@@ -29,7 +29,107 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.gbToday = new System.Windows.Forms.GroupBox();
+            this.lblToday = new System.Windows.Forms.Label();
+            this.lblTodayWeather = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.favoriteCityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gbToday.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // gbToday
+            // 
+            this.gbToday.BackColor = System.Drawing.Color.Lavender;
+            this.gbToday.Controls.Add(this.label1);
+            this.gbToday.Controls.Add(this.pictureBox1);
+            this.gbToday.Controls.Add(this.lblTodayWeather);
+            this.gbToday.Controls.Add(this.lblToday);
+            this.gbToday.Location = new System.Drawing.Point(75, 278);
+            this.gbToday.Name = "gbToday";
+            this.gbToday.Size = new System.Drawing.Size(274, 360);
+            this.gbToday.TabIndex = 0;
+            this.gbToday.TabStop = false;
+            // 
+            // lblToday
+            // 
+            this.lblToday.AutoSize = true;
+            this.lblToday.Font = new System.Drawing.Font("Arial Rounded MT Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblToday.Location = new System.Drawing.Point(91, 16);
+            this.lblToday.Name = "lblToday";
+            this.lblToday.Size = new System.Drawing.Size(102, 33);
+            this.lblToday.TabIndex = 0;
+            this.lblToday.Text = "Today";
+            // 
+            // lblTodayWeather
+            // 
+            this.lblTodayWeather.AutoSize = true;
+            this.lblTodayWeather.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTodayWeather.Location = new System.Drawing.Point(39, 295);
+            this.lblTodayWeather.Name = "lblTodayWeather";
+            this.lblTodayWeather.Size = new System.Drawing.Size(199, 31);
+            this.lblTodayWeather.TabIndex = 1;
+            this.lblTodayWeather.Text = "Today Weather";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(36, 96);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(114, 112);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(167, 204);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "°C";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 48.47561F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 51.52439F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 162F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 152F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(440, 277);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(643, 208);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripMenuItem,
+            this.favoriteCityToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1165, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.homeToolStripMenuItem.Text = "Home";
+            // 
+            // favoriteCityToolStripMenuItem
+            // 
+            this.favoriteCityToolStripMenuItem.Name = "favoriteCityToolStripMenuItem";
+            this.favoriteCityToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.favoriteCityToolStripMenuItem.Text = "Favorite City";
             // 
             // MainForm
             // 
@@ -37,16 +137,35 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1165, 749);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.gbToday);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "MainForm";
             this.Text = "WeatherMelon";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.gbToday.ResumeLayout(false);
+            this.gbToday.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbToday;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblTodayWeather;
+        private System.Windows.Forms.Label lblToday;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem favoriteCityToolStripMenuItem;
     }
 }
 
